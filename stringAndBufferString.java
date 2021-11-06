c54058e62c1879d67128d9b8cdb10dfe788cd789
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-06 22:19:41 +0530

    Add files via upload

---
+++ stringAndBufferString.java
@@ -0,0 +1,10 @@
+public class stringAndBufferString{
+public static void main(String[] args){
+    String name =new String("srikanth");
+    name.concat("boddepalli");
+    StringBuffer Name =new StringBuffer("srikanth");
+    Name.append("boddepalli");
+System.out.println(name);
+System.out.println(Name);
+}
+}
