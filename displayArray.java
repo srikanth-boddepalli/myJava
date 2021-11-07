2e28139ce2a8a9bbe3422e509db11fd80026b32a
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-07 21:45:01 +0530

    Add files via upload

---
+++ displayArray.java
@@ -0,0 +1,11 @@
+public class displayArray{
+public static void main(String[] args){
+    int[] a = {5,6,2,8,9};
+    System.out.println(a[0]);
+    System.out.println(a[1]);
+    System.out.println(a[2]);
+    System.out.println(a[3]);
+    System.out.println(a[4]);
+    System.out.println(a);
+  }
+ }
