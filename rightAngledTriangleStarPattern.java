88dc3c650362c3f6333cd821ba353005cdce24f5
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-08 15:46:43 +0530

    Add files via upload

---
+++ rightAngledTriangleStarPattern.java
@@ -0,0 +1,15 @@
+public class rightAngledTriangleStarPattern 
+{
+ public static void main(String[] args)
+ {
+  int a = 5;
+  for(int i = 1 ; i <= 5 ; i++)
+  {
+   for(int j = 1 ; j <= i ; j++)
+   { 
+    System.out.print("*");
+   }
+  System.out.println("\n");
+  }
+ }
+} 
