b1c346b9af5200994f3b9c5d80174a677a1244db
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-08 16:53:20 +0530

    Add files via upload

---
+++ lengthOfSentence.java
@@ -0,0 +1,9 @@
+public class lengthOfSentence
+{
+ public static void main(String[] args)
+ {
+  String sentence = "my name is srikanth" ;
+  int a = sentence.length();
+  System.out.println(a);
+ }
+}
