7fafc2cfb2371f7bcb22aad35e8c60f3eba7ca57
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-08 10:36:52 +0530

    Add files via upload

---
+++ sumOfArray.java
@@ -0,0 +1,15 @@
+public class sumOfArray
+  {
+    public static void main(String[] args)
+   {
+    int[] array = {3,2,4,1};
+    int total_sum =0; 
+    for(int i = 0 ; i <=3; i++){
+    
+    total_sum = total_sum+array[i];
+
+    System.out.println(total_sum);
+   } 
+    System.out.println(total_sum);
+   }
+  }
