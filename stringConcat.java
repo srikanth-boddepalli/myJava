a9e2d27b1b242b74a2ac91e47d8cd96789eb26a7
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-08 08:41:34 +0530

    Add files via upload

---
+++ stringConcat.java
@@ -0,0 +1,8 @@
+public class stringConcat 
+  {
+    public static void main(String[] args)
+    {
+    String name = new String("srikanth");
+    System.out.println(name.concat(" boddepalli"));
+    }
+  }
