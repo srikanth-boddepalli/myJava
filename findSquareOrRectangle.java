28a36a793fd2fbdc94401c9ef796ffc8dce41140
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-09 10:41:33 +0530

    Add files via upload

---
+++ findSquareOrRectangle.java
@@ -0,0 +1,17 @@
+//Take values of length and breadth of a rectangle from user and check if it is square or not.
+public class findSquareOrRectangle
+{
+ public static void main(String[] args)
+  {
+   int length = 8 ;
+   int breadth  = 5;
+   if(length == breadth)
+   {
+    System.out.println("it is square");
+   }
+   else
+   {
+    System.out.println("it is rectangle");
+   }
+  }
+}
