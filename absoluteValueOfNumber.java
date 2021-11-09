b483d2bea6649f552e569b657cdd859dcbf7b5f0
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-09 12:40:38 +0530

    Add files via upload

---
+++ absoluteValueOfNumber.java
@@ -0,0 +1,18 @@
+//Write a program to print absolute vlaue of a number entered by user. 
+public class absoluteValueOfNumber
+{
+ public static void main(String[] args)
+ {
+  int number = -5 ;
+  if (number > 0)
+  {
+   System.out.println(number);
+  }
+  else
+  {
+   System.out.println(number*(-1));
+  }
+ }
+}
+
+  
