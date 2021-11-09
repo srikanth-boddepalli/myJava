663373fd78b6432ab2fb76c9189a0beaa8087e51
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-09 08:56:33 +0530

    Add files via upload

---
+++ ifElseCase.java
@@ -0,0 +1,18 @@
+public class ifElseCase
+{
+ public static void main(String[] args)
+ {
+  int menAge = 24 ;
+  int womenAge = 20 ;
+  int ageLim = 18 ;
+  if(menAge >= ageLim && womenAge >= ageLim)
+   {
+    System.out.println("ready for marriage");
+   }
+  else
+   {
+    System.out.println("not ready for marriage");
+   }
+  System.out.println("Over");
+  }
+}
