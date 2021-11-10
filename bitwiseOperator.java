dbe3c2e412e9a8eefaad2d2f789e692e3a1a1f20
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-10 09:01:34 +0530

    Add files via upload

---
+++ bitwiseOperator.java
@@ -0,0 +1,12 @@
+public class bitwiseOperator
+{
+ public static void main(String[] args)
+ {
+// bitwise "and" operator is "&"
+//bitwise "or" operator is "|"
+//bitwise "not" operator is "^"
+  System.out.println(12&7);
+  System.out.println(12^7);
+  System.out.println(12|7);
+ }
+}
