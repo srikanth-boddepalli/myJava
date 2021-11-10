ca333a934746fd81bcc1ac8a21dc471f0c4b78df
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-10 11:13:12 +0530

    Add files via upload

---
+++ characterIsVowelOrConsonant.java
@@ -0,0 +1,17 @@
+//check whether the character is vowel or consonant
+
+public class characterIsVowelOrConsonant
+{
+ public static void main(String[] args)
+ {
+  char a = 'e' ;
+  if( a == 'a'||a == 'e'||a == 'i'||a == 'o'||a == 'u')
+  {
+   System.out.println(a + " is vowel");
+  }
+  else
+  {
+   System.out.println(a + " is Consonant");
+  }
+ }
+}
