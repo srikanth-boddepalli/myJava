14a8f477ab1dee5f7f3809530e6343f8a1c2fff1
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-10 11:08:41 +0530

    Add files via upload

---
+++ floatingPointNumbersAndTestsWhetherWheyAreTheSame.java
@@ -0,0 +1,18 @@
+// Write a Java program that reads in two floating-point numbers and tests whether they are the same up to three decimal places
+
+public class floatingPointNumbersAndTestsWhetherWheyAreTheSame
+{
+ public static void main(String[] args)
+ {
+  double a = 24.346 ;
+  double b = 24.345 ;
+  if(a == b )
+  {
+   System.out.println(a + " and " + b + "are same.");
+  }
+  else
+  {
+   System.out.println(a + " and " + b + "are diffrent.");
+  }
+ }
+}
