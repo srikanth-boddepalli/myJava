d21943d155565511a59cbaaa5be9c2bb269b8d62
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-14 17:23:25 +0530

    Add files via upload

---
+++ print1to100ByUseingWhile.java
@@ -0,0 +1,12 @@
+public class print1to100ByUseingWhile
+{
+ public static void main(String[] args)
+ {
+  int num = 1 ;
+  while(num <= 100 )
+  {
+   System.out.println(num);
+   num++;
+  }
+ }
+} 
