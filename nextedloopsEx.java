e4c73880328d93a0c842287d65ca76a4ce5f96fe
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-15 22:19:30 +0530

    Add files via upload

---
+++ nextedloopsEx.java
@@ -0,0 +1,16 @@
+public class nextedloopsEx
+{
+ public static void main(String[] args)
+ {
+  for(int i = 1 ; i<= 5 ; i++)
+  {
+   System.out.println("start");
+   for(int j = 1 ; j <= 3 ; j++)
+   {
+    System.out.println("do"); 
+   }
+   System.out.println("rest");
+   System.out.println();   
+  }
+ }
+}    
