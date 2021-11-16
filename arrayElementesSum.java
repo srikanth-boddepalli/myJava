2de1526d79dfadded9039a1311345cb45a267de4
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-16 16:23:45 +0530

    Add files via upload

---
+++ arrayElementesSum.java
@@ -0,0 +1,13 @@
+public class arrayElementesSum
+ {
+ public static void main(String[] args)
+ {
+  int[] a = {2,5,6,3};
+  int sum = 0 ;
+  for(int i=0 ;i <= 3;i++)
+  {
+   sum = sum + a[1] ;
+  }
+  System.out.println(sum);
+ }
+}
