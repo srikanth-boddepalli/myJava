2dc97b3aae34bad022081cfeeca655beda1e5a96
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-16 16:26:22 +0530

    Add files via upload

---
+++ asciiValueOfACharacter.java
@@ -0,0 +1,16 @@
+//print the ascii value of a character
+public class asciiValueOfACharacter
+{
+ public static void main(String[] args)
+ {
+  int  a = 'a' ;
+  char ch = 'a' ;
+  while(a <= 'z')
+  {
+   System.out.println(a + "  " + ch);
+   a++;
+   ch++;
+  }
+ }
+}
+  
