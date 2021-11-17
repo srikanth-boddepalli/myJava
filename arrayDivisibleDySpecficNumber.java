c6a96d54d2dcccc2e5971e1ec589ec3051f4e632
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-17 11:37:14 +0530

    Add files via upload

---
+++ arrayDivisibleDySpecficNumber.java
@@ -0,0 +1,13 @@
+//numder  that are divisible by specfic number in an array
+public class arrayDivisibleDySpecficNumber
+{
+ public static void main(String[] args)
+ {
+  int[] a = {2,4,6,8};
+  int num = a.length ;
+  int div = 2 ; 
+  int result ; 
+  result = a[2]/2 ; 
+  System.out.println(result);
+ }
+}
