063f5c72aa0ae7a077c8a104991351278f4661fd
srikanth-boddepalli <93824876+srikanth-boddepalli@users.noreply.github.com>
2021-11-23 15:42:47 +0530

    Add files via upload

---
+++ addConcatByUseingMethods.java
@@ -0,0 +1,18 @@
+class  addConcatByUseingMethods
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		add(10,20);
+		concat("Srikanth",9505449542l);
+	}
+	public static void add(int a , int b) 
+	{
+		System.out.println(a+b);
+	}
+		public static void concat(String c , long d) 
+	{
+		System.out.println(c + d);
+	}
+		
+}
